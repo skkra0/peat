dcb845c5c9c0f92bf7c24e37655d59bfc58cffa0
Emma Zheng <73250770+skkra0@users.noreply.github.com>
2024-04-11 17:10:59 +0000

    taskform component and checkboxes

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,47 @@
 "use client"
 import React, {FormEvent, useState} from 'react';
-interface Task {
-    title: string;
-    description: string;
-}
+import TaskForm from './taskform';
+
 const page = () => {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
     const [tasks, setTasks] = useState<Task[]>([]);
 
-    const submitHandler = (e: FormEvent) => {
+    const [formTask, setFormTask] = useState<Task>({
+        title: "",
+        description: "",
+        steps: [],
+    });    
+
+    const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
-        setTasks([...tasks, {title, description}])
-        setTitle("");
-        setDescription("");
-        console.log(tasks)
+        setTasks([...tasks, formTask]);
+        setFormTask({
+            title: "",
+            description: "",
+            steps: [],
+        });
+        console.log(tasks);
     }
     
     let renderTasks : JSX.Element[] = [<h2 className="text-xl">No Tasks</h2>];
 
     renderTasks = tasks.map((task, index) => {
         return (
-            <div key={index} className="bg-orange-300 w-full aspect-square text-slate-700 rounded-lg p-3 inline-block">
+            <div key={`task-${index}`} className="bg-orange-300 w-full aspect-square text-slate-700 rounded-lg p-3 inline-block">
                 <h5 className = "font-semibold text-xl">{task.title}</h5>
                 <h6 className="break-words">{task.description}</h6>
+                <ol>
+                    {task.steps.map((step, i) => {
+                        return (
+                            <li key={`step-${index}-${i}`}>
+                                <input 
+                                type="checkbox"
+                                id={`step-checkbox-${index}-${i}`}
+                                className="mr-2"/>
+                                <label htmlFor={`step-checkbox-${index}-${i}`}>{step}</label>
+                            </li>
+                        );
+                    })}
+                </ol>
             </div>
         );
     });
@@ -35,46 +53,13 @@ const page = () => {
             <button className="btn inline-block float-right">+</button>
         </div>
         <div className="bg-gradient-to-r from-emerald-600 to-amber-500 h-2"></div>
-        <div className="main text-black bg-slate-50" >
-            <form onSubmit={submitHandler}>
-                <input 
-                    type="text"
-                    className="border-b-2 border-zinc-400 py-1 m-2 block"
-                    placeholder="List Title"
-                    value={title}
-                    onChange={
-                        (e) => {
-                            setTitle(e.target.value);
-                        }
-                    }
-                />
-                <textarea
-                    className="border-b-2 border-zinc-400 py-1 m-2 h-20 block break-words resize-none"
-                    placeholder="List Description"
-                    value={description}
-                    onChange={
-                        (e) => {
-                            setDescription(e.target.value);
-                        }
-                    }
-                />
-                <div>
-                <div>
-                        <input 
-                        className="border-zinc-400 border-b-2 py-1 m-2" 
-                        placeholder="Add to-do item"/>
-                    <button 
-                        type="button"
-                        className="bg-slate-700 text-white rounded py-1 px-2"
-                    >+</button>
-                </div>
-                <button
-                    className="bg-slate-700 text-white rounded py-1 px-2"
-                >Add Task
-                </button>
-                </div>
-            </form>
-            <div className="m-6 grid grid-cols-4 gap-6">
+        <div className="main text-black bg-slate-50 pl-4 pr-4" >
+            <TaskForm
+                formTask={formTask}
+                setFormTask={setFormTask}
+                handleSubmit={handleSubmit}
+            />
+            <div className="grid grid-cols-4 gap-6">
                 {renderTasks}
             </div>
         </div>
